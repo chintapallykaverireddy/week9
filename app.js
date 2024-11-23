7f024978ba1e8a8d2c3a3f36c1f1d404f78fef74
Kaveri Reddy <123486234+chintapallykaverireddy@users.noreply.github.com>
2024-11-23 09:55:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello, Docker!');
 });
 
-app.listen(3005, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
